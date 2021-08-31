ab7038530cae36b51dc672c536c906bf46dc4f8c
Hannah Lappe <hannah.lappe0@gmail.com>
2021-08-31 02:17:52 +0200

    auto update

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 
-polybar -r primary &> /dev/null & disown
+#polybar -r primary &> /dev/null & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
